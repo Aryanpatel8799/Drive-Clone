814e09213e4b1ac95c751431033923072a90f892
Aryanpatel8799 <amp8799@gmail.com>
2025-02-22 14:42:21 +0530

    user sign in done using JWT cookie parser and Bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const dotenv=require('dotenv');
 dotenv.config();
 const connectDB = require('./config/db');
 connectDB();
+const cookieparser=require('cookie-parser');
 
+app.use(cookieparser());
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -3,11 +3,12 @@ const routes = express();
 const { body,validationResult } = require('express-validator');
 const userModel = require('../models/userModels');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 routes.get('/register',(req,res)=>
     {
-        res.render('register');
+        res.render('register'); 
     })
     
 routes.post('/register',
@@ -34,5 +35,50 @@ routes.post('/register',
     res.json(user);
 
 })
+
+routes.get('/login',(req,res)=>
+{
+    res.render('login');
+})
+
+routes.post('/login',
+    body('email').trim().isEmail().isLength({min:13}),
+    body('password').trim().isLength({min:8}),
+    async (req,res)=> {
+        
+        const error=validationResult(req);
+        if(!error.isEmpty()){
+            return res.status(400).json({errors:error.array(),message:'Validation Error'});
+        }
+
+        const {email,password}=req.body;
+        
+        const user=await userModel.findOne({email:email});
+        
+        if(!user)
+        {
+            return res.status(400).json({message:'user not found',status:400});
+        }
+
+        const isUser=await bcrypt.compare(password,user.password);
+
+        if(!isUser)
+        {
+            res.status(400).json({message:'Username or password is incorrect'})
+        }
+
+        const token=jwt.sign({
+            userId:user._id,
+            email:user.email,
+            name:user.name
+        },
+        process.env.SECRET_KEY,
+        )
+
+        res.cookie('token',token);
+
+        res.send('login successful');
+    }
+)
 
 module.exports=routes;
